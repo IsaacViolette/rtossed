72d007bf0accfa8404179e606dc69b5fa1e6445e
Isaac Violette <isaac.violette@maine.edu>
2023-02-07 17:53:47 -0500

    Finished syscalls, for reading and writing chars

--- Core/Src/syscalls.c
+++ Core/Src/syscalls.c
@@ -1,3 +1,7 @@
+#include <stddef.h>
+#include <stdint.h>
+#include "usart.h"
+
 int _write(int file, void *ptr, size_t len)
 {
  	// Transmit printf string to USART - blocking call
@@ -10,7 +14,14 @@ _ssize_t _write_r (struct _reent *ptr, int fd, const void *buf, size_t cnt)
 	return _write(fd, (char *)buf, cnt);
 }
 
+
 _ssize_t _read_r(struct _reent *ptr, int fd, void *buf, size_t cnt)
 {
-
+	HAL_StatusTypeDef ret;
+	do {
+		ret = HAL_UART_Receive(&huart3, (uint8_t *) buf, 1, HAL_MAX_DELAY);
+	} while(ret != HAL_OK);
+	
+	return 1;
 }
+
