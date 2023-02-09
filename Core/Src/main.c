80ccce5e7dbca5f902040fbf627aaf8b1bed0c02
Isaac Violette <isaac.violette@maine.edu>
2023-02-08 20:25:59 -0500

    Code complete with comments

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -66,60 +66,71 @@ void SystemClock_Config(void);
 
 /* USER CODE END 0 */
 
-/**
-  * @brief  The application entry point.
-  * @retval int
-  */
-
-/*This function returns an array of characters which a user inputs through the command line*/
+/*This function modifies a passed array of characters*/
+/*which stores user input */
 void getline_sh(char buf[])
 {
 	int i = 0;
 	char input;
 
-	while(1) {
+	while (1) {
 		input = getchar();
-		if(input != '\b') {
-
-			if((input == '\r') || (input == '\n'))	
+		/*If the input is not a backspace, print the*/
+		/*character and store character in array*/
+		if (input != '\b') {
+			
+			/*When the enter key is pressed, there is no more user input*/
+			if ((input == '\r') || (input == '\n'))	
 				break;
 			else {
+				/*Print the character and store int in array*/
 				printf("%c",input);
 				buf[i] = input;
-				if(i == 510)
+
+				/*If the user input is too large,*/
+				/*overwrite the last character with*/
+				/*whatever the next user character is*/
+				if (i == 510)
 					i--;
 				i++;
 			}
 		}
-
+		
+		/*When the backspace is pressed and the array has items in it,*/
+		/*remove the character from the screen and in the array*/
 		if ((input == '\b') && (i != 0)) {
-			printf("%c",input);
-			i--;
-			buf[i] = '\0';
-			printf(" \b");
+			printf("%c",input); 
+			i--; 
+			buf[i] = '\0'; //remove the value the was backspaced from the array
+			printf(" \b"); //this makes the display show the delete
 		}
-		if ((input == '\b') && (i == 0))
-			i = 0;
+		if ((input == '\b') && (i == 0));
 	}
 	printf("\n\r");
-	if(i != 510)
+
+	/*Make sure that after the array is stored, the last character is Null for string management*/
+	if (i != 510)
 		buf[i] = '\0';
 	else
 		buf[511] = '\0';
 }
 
-
+/*This function runs the shell which receives a line of input.*/
+/*Checks the input for a leading echo command. If true, print */
+/*the following user input. Otherwise, return out of the shell.*/
 int sh()
 {
-	char buf[512];
-	char print_buf[512];
-	printf("$ ");
-	getline_sh(buf);
+	char buf[512]; //this array is used for user input
+	char print_buf[512]; //this array is used for storing characters after 'echo' command
+	printf("$ "); //shell always leads with $
+	getline_sh(buf); //receive user input into buf array
 
-	if(strncmp(buf,"echo ",5) == 0)
+	if (strncmp(buf,"echo ",5) == 0)
 	{
+		/*Cycle through buf after 'echo ', and store it into print_buf array*/
+		/*making sure that the last character is /0*/
 		for (int i = 0; i < 512; i++){
-			if(buf[i+5] == '\0') {
+			if (buf[i+5] == '\0') {
 				print_buf[i] = '\0';
 				break;
 			}
@@ -128,7 +139,7 @@ int sh()
 		printf("%s\n\r", print_buf);
 	}
 
-	return 1;
+	return 0;
 }
 
 
@@ -182,6 +193,7 @@ int main(void)
 
 
 	/* USER CODE BEGIN WHILE */
+	/*Run the shell program continuously*/
 	while (1) {
 		sh();
 	}
