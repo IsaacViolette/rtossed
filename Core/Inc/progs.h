033934a63bf03378ac60459a91997fa1702354cf
Isaac Violette <isaac.violette@maine.edu>
2023-02-20 15:47:00 -0500

    Comment change

--- Core/Inc/progs.h
+++ Core/Inc/progs.h
@@ -5,11 +5,8 @@
 #ifndef __PROGS_H
 #define __PROGS_H
 
-/*Indicated that the system is initialized and user spece process may start*/
-extern int kready;
-
 void process1(void);
 
 void process2(void);
 
-#endif /*__MAIN_H*/
+#endif /*__PROGS_H*/
