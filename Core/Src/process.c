3c329915df51436e121369d5b886ca90c39ecd96
Isaac Violette <isaac.violette@maine.edu>
2023-02-21 16:59:05 -0500

    Global variables are written

--- Core/Src/process.c
+++ Core/Src/process.c
@@ -1,5 +1,10 @@
 /*Includes*/
 #include "process.h"
 
-/*Global Variables*/
+/*Defines*/
+#define PROC_MAX 4;
 
+/*Global Variables*/
+struct task_struct process_table[PROC_MAX];
+struct task_struct task_idle;
+struct task_struct *current = &task_idle;
