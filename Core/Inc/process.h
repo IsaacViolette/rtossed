c246ead46233b45c5a6698079779ea5dd5ec352d
Isaac Violette <isaac.violette@maine.edu>
2023-02-21 16:27:28 -0500

    Task Structure is created and can compile

--- Core/Inc/process.h
+++ Core/Inc/process.h
@@ -23,15 +23,41 @@ static inline void yield(void)
 	__ISB();
 }
 
+/*Structure of core STM32M7 registers to save */
+struct __attribute__ ((__packed__)) registers {
+	uint32_t r0;
+	uint32_t r1;
+	uint32_t r2;
+	uint32_t r3;
+	uint32_t r4;
+	uint32_t r5;
+	uint32_t r6;
+	uint32_t r7;
+	uint32_t r8;
+	uint32_t r9;
+	uint32_t r11;
+	uint32_t r12;
+	uint32_t SP;
+	uint32_t LR;
+	uint32_t PC;
+	uint32_t PSR;
+	uint32_t PRIMASK;
+	uint32_t FAULTMASK;
+	uint32_t BASEPRI;
+	uint32_t CONTROL;
+	uint32_t xPSR;
+};
+
 struct __attribute__ ((__packed__)) task_struct {
 	uint32_t state;
 	pid_t pid; //unique integer
 	uint32_t exc_return; //Special program counter value that indicates how an exception handler returns
 	uint32_t sp_start; //starting stack pointer address
 	uint32_t (*cmd)(void); //pointer to a function
-	struct r; //Structure that contains saved registers
+	struct registers r; //Structure that contains saved registers
 
 };
+
 
 
 #endif /*__PROCESS_H*/
