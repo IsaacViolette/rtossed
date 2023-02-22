6b7cd61678244bc7c239535a45edf645cc905dcc
Isaac Violette <isaac.violette@maine.edu>
2023-02-21 22:08:37 -0500

    After many compilation errors, finaly casted stack register right, now to do it for all pointers

--- Core/Src/process.c
+++ Core/Src/process.c
@@ -4,7 +4,15 @@
 /*Defines*/
 #define PROC_MAX 4
 
+//extern const uint32_t _eustack[];
+
 /*Global Variables*/
 struct task_struct process_table[PROC_MAX];
 struct task_struct task_idle;
 struct task_struct *current = &task_idle;
+
+void process_stack_init(struct task_struct *init)
+{
+	*((uint32_t*)init->r.SP-1) = 0;
+
+}
