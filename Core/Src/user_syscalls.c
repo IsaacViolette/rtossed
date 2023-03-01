0eed2d3f78142f34f5a1eda8306e82e4d562611f
Isaac Violette <isaac.violette@maine.edu>
2023-02-28 22:28:01 -0500

    Made sure it compiled successfully

--- Core/Src/user_syscalls.c
+++ Core/Src/user_syscalls.c
@@ -4,7 +4,7 @@
 
 void microsleep(uint32_t sleep_amount)
 {
-	current->w_time = wTick + (sleep_amount/1000);
+	current->w_time = uwTick + (sleep_amount/1000);
 	current->state |= STATE_TIME_SLEEP;
 
 	yield();
