8cb1b845f81bd9e74891afd8007da836085b3b8a
Isaac Violette <isaac.violette@maine.edu>
2023-04-09 14:25:25 -0400

    added an increment to see how process calls interact

--- Core/Src/progs.c
+++ Core/Src/progs.c
@@ -5,9 +5,14 @@
 /*Blink the onboard green LED*/
 int process1(void)
 {
+	static uint8_t i; //project 2 increment
 	while (1) {
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
 		microsleep(500000);
+
+		i++;
+
+		printf("Current: %d | The current calue of i is: %u\r\n",current->pid,i);
 	}
 
 	return 0;
