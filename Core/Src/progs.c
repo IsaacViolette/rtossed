af4c6c2334ec216577fbb0dcc5c94a4161f97a68
Isaac Sterling Violette <isaac.violette@maine.edu>
2023-04-11 19:29:10 +0000

    move increment inside of lock

--- Core/Src/progs.c
+++ Core/Src/progs.c
@@ -12,14 +12,14 @@ int process1(void)
 	while (1) {
 		microsleep(1000);
 		HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_0);
-
-		i++;
 		
 		/*Block if no lock is availible, 9 was used randomly for lock ID*/
 		while (HAL_HSEM_Take(9, current->pid) != HAL_OK) {
 			yield(); //Go to next process
 		}
 
+		i++;
+		
 		/*Printed onto minicom at 2400 baud rate, used to check if locking is working*/
 		printf("Current: %d | The current value of i is: %u\r\n",current->pid,i);
 
